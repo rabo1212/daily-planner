ab8183fc6dfc593febadaa33b66ebb7be78a0b93
rabo1212 <h311015330@gmail.com>
2026-01-10 17:11:52 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker - 간단 버전
+const CACHE_NAME = "daily-planner-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/daily-planner/",
+        "/daily-planner/index.html",
+        "/daily-planner/manifest.json",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
